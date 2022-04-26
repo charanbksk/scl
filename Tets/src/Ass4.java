8bbebdbd48f16e57445599eae551300ff1e67bba
unknown <charan.kotikala@gmail.com>
2022-04-26 16:49:20 +0200

    first

---
+++ Tets/src/Ass4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ass4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		@SuppressWarnings("resource")
+		Scanner sc= new Scanner(System.in);
+		 String a = sc.nextLine();
+	     String b = sc.nextLine();
+	     System.out.println("Hello" +" " +a);
+	     System.out.println("hello" +" " +b);
+	
+}
+
+}
